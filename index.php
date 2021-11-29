20c90db45aba4b6042d3f04f3df5b926d610519c
greicewelter <greicewelter8@gmail.com>
2021-11-29 20:54:14 -0300

    Criaçao da tela de login, falta ajustar

--- index.php
+++ index.php
@@ -17,6 +17,47 @@
         <h1>Venha Investir com a gente</h1>
         <h1>Easier Bank, aqui você vem primeiro</h1>
     </div>
+    <br>
+    
+<center>
+            <form class="card">
+
+                <div class="card-header">
+                    
+                    <h2>Login</h2>
+                   
+                </div>
+
+                <div class="text-center">
+
+                    <div class="card-content-area">
+
+                        <label for="usuario">Usuário</label>
+
+                        <input type="text" id="usuario" autocomplete="off">
+
+                    </div>
+
+                    <div class="card-content-area">
+
+                        <label for="password">Senha</label>
+
+                        <input type="password" id="password" autocomplete="off">
+
+                    </div>
+
+                </div>
+
+                <div class="card-footer">
+
+                    <input type="submit" value="login" class="submit">
+
+                    <a href="#" class="recuperar_senha">Esqueceu a senha?</a>
+
+                </div>
+
+            </form>
+            </center>
 
     <footer class="pt-4 my-md-5 pt-md-5 border-top text-center">
         <div class="row">
