a90b5a455976240b9fc63f7bd24842bb0efb52f6
greicewelter <greicewelter8@gmail.com>
2021-12-01 10:46:58 -0300

    atualizaçoes

--- includes/session.php
+++ includes/session.php
@@ -1,6 +1,11 @@
 <?php
 print_r($_SESSION);
 if (!isset($_SESSION['usuario'])) {
+    $menssage = [
+        'tipo' => 'alert-warning',
+        'mensagem' => 'Efetue o login para acessar!',
+    ];
+    $_SESSION['alert'] = serialize($menssage);
     header("Location: /index.php");
 }
 
