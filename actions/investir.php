0ff6d8f3bb86cd5a79b8d998e047492288a90558
Elias <eliasbekovski@gmail.com>
2021-12-01 22:36:01 -0300

    Ajustes

--- actions/investir.php
+++ actions/investir.php
@@ -1,63 +1,62 @@
  <?php
-include_once '../includes/start.php';
-include_once '../includes/conecta.php';
-include_once '../includes/session.php';
+    include_once '../includes/start.php';
+    include_once '../includes/conecta.php';
+    include_once '../includes/session.php';
 
-$valor = $_POST['valor'];
-$investimento = $_POST['investimento'];
+    $valor = $_POST['valor'];
+    $investimento = $_POST['investimento'];
 
-$consulta = $con->prepare("select * from investimentos where id = '$investimento'");
-$consulta->execute();
-$result = $consulta->fetch(PDO::FETCH_ASSOC);
+    $consulta = $con->prepare("select * from investimentos where id = '$investimento'");
+    $consulta->execute();
+    $result = $consulta->fetch(PDO::FETCH_ASSOC);
 
-if(empty($result)){
-    $menssage = [
-        'tipo' => 'alert-warning',
-        'mensagem' => 'Investimento não encontrado!',
-    ];
-
-
-    $_SESSION['alert'] = serialize($menssage);
-
-    header("Location: /investimento.php");
-    die;
-}
-if(floatval($valor)< floatval($result['valor_minimo'])){
-    $menssage = [
-        'tipo' => 'alert-warning',
-        'mensagem' => 'Valor inferior do minimo recomendado!',
-    ];
+    if (empty($result)) {
+        $menssage = [
+            'tipo' => 'alert-warning',
+            'mensagem' => 'Investimento não encontrado!',
+        ];
 
 
-    $_SESSION['alert'] = serialize($menssage);
+        $_SESSION['alert'] = serialize($menssage);
 
-    header("Location: /investir.php?id=$investimento");
-    die;   
-}
+        header("Location: /investimento.php");
+        die;
+    }
 
-$consulta = $con->prepare("select sum(valor) as total from carteiras where cliente_id = '{$usuario['id']}'");
-$consulta->execute();
-$carteira = $consulta->fetch(PDO::FETCH_ASSOC);
+    if (floatval($valor) < floatval($result['valor_minimo'])) {
+        $menssage = [
+            'tipo' => 'alert-warning',
+            'mensagem' => 'Valor inferior do minimo recomendado!',
+        ];
 
+        $_SESSION['alert'] = serialize($menssage);
 
+        header("Location: /investir.php?id=$investimento");
+        die;
+    }
 
-if(floatval($usuario['saldo'])< floatval($carteira['total'])){
-    $menssage = [
-        'tipo' => 'alert-warning',
-        'mensagem' => 'Saldo insuficiente!',
-    ];
+    $consulta = $con->prepare("select sum(valor) as total from carteiras where cliente_id = '{$usuario['id']}'");
+    $consulta->execute();
+    $carteira = $consulta->fetch(PDO::FETCH_ASSOC);
 
+    if (floatval($usuario['saldo']) < (floatval($carteira['total']) + floatval($valor))) {
+        $saldoDisponivel = floatval($usuario['saldo']) - floatval($carteira['total']);
+        $saldoDisponivel = $saldoDisponivel < 0 ? 0 : $saldoDisponivel;
+        $menssage = [
+            'tipo' => 'alert-warning',
+            'mensagem' => "Saldo insuficiente! Saldo Disponível: R$ $saldoDisponivel",
+        ];
 
-    $_SESSION['alert'] = serialize($menssage);
+        $_SESSION['alert'] = serialize($menssage);
 
-    header("Location: /investir.php?id=$investimento");
-    die;   
-}
-
+        header("Location: /investir.php?id=$investimento");
+        die;
+    }
 
 
 
-print_r($usuario);
-print_r($_POST);
-print_r($result); 
-print_r($carteira);
+
+    print_r($result);
+    // print_r($_POST);
+    // print_r($usuario);
+    // print_r($carteira);
--- carteira.php
+++ carteira.php
@@ -1,4 +1,4 @@
-<?php include_once './includes/start.php'; 
+<?php include_once './includes/start.php';
 include_once './includes/session.php';
 include_once './includes/conecta.php';
 
@@ -6,7 +6,7 @@ include_once './includes/conecta.php';
 $consulta = $con->prepare("select * from carteiras ");
 $consulta->execute();
 $result = $consulta->fetchAll();
-print_r($result);
+// print_r($result);
 
 
 ?>
@@ -29,12 +29,36 @@ print_r($result);
     <?php include_once './includes/alert.php' ?>
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <p class="lead">
-            Carteira
+        <p class="h2">
+            <b>Minha Carteira</b>
         </p>
     </div>
 
     <div class="container">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col" class="text-center">Investimento</th>
+                    <th scope="col" class="text-center">Valor</th>
+                    <th scope="col" class="text-center">Data Inicial</th>
+                    <th scope="col" class="text-center">Data Final</th>
+                    <th scope="col" class="text-center"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row" class="align-middle">1</th>
+                    <td class="align-middle">Lideranças Femininas</td>
+                    <td class="text-right align-middle">R$ 100</td>
+                    <td class="text-right align-middle">10/02/2021</td>
+                    <td class="text-right align-middle">10/02/2022</td>
+                    <td class="text-center align-middle">
+                        <a class="btn btn-easier mr-2" href="/investir.php?id=<?php echo 'f'; ?>">Detalhes</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 
     <?php include_once './includes/footer.php' ?>
